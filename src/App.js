a8aaad7f80bcf70d91792c1a501c4945e9b22017
Piotr Gaik <piotgie@gmail.com>
2021-06-05 17:18:15 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Header } from './components/Header'
+import { Watchlist } from './components/Watchlist'
+import { Watched } from './components/Watched'
+import { Add } from './components/Add'
+import './lib/font-awesome/css/all.min.css'
+import { GlobalProvider } from './context/GlobalState'
+ 
+function App() {
+  return (
+    <GlobalProvider>
+
+
+      <Router>
+
+        <Header />
+
+        <Switch>
+
+          <Route exact path="/">
+            <Watchlist />
+          </Route>
+
+          <Route path="/watched">
+            <Watched />
+          </Route>
+
+          <Route path="/add">
+            <Add />
+          </Route>
+
+        </Switch>
+        
+      </Router>
+
+    </GlobalProvider>
+  );
+}
+
+export default App;
